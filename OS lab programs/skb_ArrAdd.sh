6ac128d5dd2c697fe2ade9c2236b797a682d158a
Suryakant Bharti <suryakant.bharti@gmail.com>
2017-10-01 22:04:43 +0530

    Update skb_ArrAdd.sh

--- OS lab programs/skb_ArrAdd.sh
+++ OS lab programs/skb_ArrAdd.sh
@@ -1,10 +1,11 @@
 'NOTE: To see what a particular program does check out the "Program List.docx" file.
-This is a program from "Operating Systems Lab" of "Shri Mata Vaishno Devi University" taught by "Dr. Ajay Koul">
-Copyright (C) 2013  Suryakant Bharti <www.suryakantbharti.com>
+This is a program from "Operating Systems Lab" of "Shri Mata Vaishno Devi University".
+@author Suryakant Bharti
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '
+
 echo enter number of elements
 read num
 echo enter the elements
@@ -14,4 +15,3 @@ read a[$i]
 sum=`expr $sum + ${a[i]}`
 done
 echo the sum of array is $sum
-
